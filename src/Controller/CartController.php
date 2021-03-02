01d2697719abd472b41153166c164b8d7018e7e1
code-larry <larry.basin@gmail.com>
2021-03-02 21:14:12 +0100

    Display of cart content

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -47,4 +47,30 @@ class CartController extends AbstractController
 			'slug' => $product->getSlug()
 		]);
     }
+
+	/**
+	 * @Route("/cart", name="cart_show")
+	 */
+	public function show(SessionInterface $session, ProductRepository $productRepository)
+	{
+		$detailedCart = [];
+		$total = 0;
+
+		foreach($session->get('cart', []) as $id => $qty)
+		{
+			$product = $productRepository->find($id);
+
+			$detailedCart[] = [
+				'product' => $product,
+				'qty' => $qty
+			];
+
+			$total += ($product->getPrice() * $qty);
+		}
+
+		return $this->render('cart/index.html.twig', [
+			'items' => $detailedCart,
+			'total' => $total
+		]);
+	}
 }
